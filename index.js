d4674154860b1a47fc503e6d8f225294c1483c62
Alexander Wallin <office@alexanderwallin.com>
2016-04-22 13:53:01 +0200

    Improvements

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var path = require('path');
 
+global.snoret = { count: 0 };
+
 module.exports = function(babel) {
   var importantMessages = [
     'Oj oj',
@@ -21,17 +23,31 @@ module.exports = function(babel) {
     visitor: {
       Program: {
         exit(node, state) {
+          if (global.snoret.count === 0) {
+            console.log('\n\nSnöre:\n');
+          }
+
           var filename = path.basename(state.file.opts.filename);
 
+          var parts = [' /', '|', ' \\', '  \\', '   |', '  /'];
+          var timestamp = Math.round(Date.now() / 1000);
+          var index = timestamp % parts.length;
+          var part = parts[global.snoret.count % parts.length];
+          var output = part;
+
           if (filename.length >= 35) {
-            console.log(filename + ' (jättelångt filnamn)');
+            output += '   ' + filename + ' (jättelångt filnamn)';
           }
           else if (/reducer\.js$/.test(filename) && Math.random() >= 0.6) {
-            console.log(filename.replace(/reducer\.js$/, 'rädisor.js'));
+            output += '   ' + filename.replace(/reducer\.js$/, 'rädisor.js');
           }
           else if (Math.random() >= 0.92) {
-            console.log(filename + '   ' + importantMessages[Math.round(Math.random() * (importantMessages.length - 1))]);
+            output += '   ' + filename + ' - ' + importantMessages[Math.round(Math.random() * (importantMessages.length - 1))];
           }
+
+          console.log(output);
+
+          global.snoret.count += 1;
         }
       }
     }
